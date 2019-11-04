b51aab817a0341dac9a94a2116bc4460de1c2af4
Mel <melissa.c.t.bower@gmail.com>
2019-11-03 22:42:52 -0700

    refactor: clean up code and add $

--- js/main.js
+++ js/main.js
@@ -1,45 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#submit").click(function(){
-      $("#response").removeClass("hidden");
-      computeTotalCost();
-  });
-$(".addDependent").click(function(){
-    $("#dependents").append("<div class=\"input-group\" id = \"dependents\"><span class=\"input-group-addon\">Dependent's Name</span><input type=\"text\" placeholder=\"Dependent\'s Name\" class=\"form-control dependent\"></input></div>");
-});
+	$("#submit").click(function() {
+		$("#response").removeClass("hidden");
+		computeTotalCost();
+	});
 
-function computeTotalCost() {
-  // var dependents = $(".dependents").val() * 500;
-  dependents = 0;
-  $(".dependent").each(function(){
-    var thisDependent = this.value.charAt(0);
-    console.log(thisDependent);
-    if(thisDependent == "a"){
-      dependents += 500*.9;
-      this.append("<span> X 10%</span>")
-    }
-    else {
-    dependents += 500
-    }
-})
-  console.log(dependents);
+	$(".addDependent").click(function() {
+		$("#dependents").append("<div class=\"input-group\" id = \"dependents\"><span class=\"input-group-addon\">Dependent's Name</span><input type=\"text\" placeholder=\"Dependent\'s Name\" class=\"form-control dependent\"></input></div>");
+	});
 
-  var employeeNameFirstLetter = $("#employeeName").val().charAt(0);
-  console.log("name " + employeeNameFirstLetter);
+	function computeTotalCost() {
+		// var dependents = $(".dependents").val() * 500;
+		dependents = 0;
+		$(".dependent").each(function() {
+			var thisDependent = this.value.charAt(0);
+			console.log(thisDependent);
+			if (thisDependent == "a") {
+				dependents += 500 * .9;
+				this.append("<span> X 10%</span>")
+			} else {
+				dependents += 500
+			}
+		})
 
-  var discount = 1000
-  if( employeeNameFirstLetter == "a"){
-    discount = .9 * 1000;
-    $("#nameStartsWithA").removeClass("hidden");
-}
-var totalCostBeforeDeductions = 2000 * 26;
-var benefitsTotalCost = dependents + discount
-var totalCost = totalCostBeforeDeductions - benefitsTotalCost;
+		var employeeNameFirstLetter = $("#employeeName").val().charAt(0);
+		var discount = 1000
+		if (employeeNameFirstLetter == "a") {
+			discount = .9 * 1000;
+			$("#nameStartsWithA").removeClass("hidden");
+		}
+		var totalCostBeforeDeductions = 2000 * 26;
+		var benefitsTotalCost = dependents + discount
+		var totalCost = totalCostBeforeDeductions - benefitsTotalCost;
 
-console.log("totalCost*discount "+totalCost +" * " + discount +" = " + totalCost);
-$("#totalCostBeforeDeductions").text( totalCostBeforeDeductions);
-$("#benefitsTotalCost").text( benefitsTotalCost );
-$("#totalCost").text(totalCost);
-}
+		$("#totalCostBeforeDeductions").text("$" + totalCostBeforeDeductions);
+		$("#benefitsTotalCost").text("$" + benefitsTotalCost);
+		$("#totalCost").text("$" + totalCost);
+	}
 
 });
